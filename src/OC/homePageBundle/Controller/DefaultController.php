c63af0d12cb1d52bce242bafd022dd75af1c86a9
Anda <ax.fiolle@gmail.com>
2018-02-05 10:07:13 +0100

    +multi_page(filrouge)
    Need vhost conf to get assets on custom domain name

--- src/OC/homePageBundle/Controller/DefaultController.php
+++ src/OC/homePageBundle/Controller/DefaultController.php
@@ -13,9 +13,27 @@ class DefaultController extends Controller
   
   public function otherAction($page) {
     switch($page){
-      case 'about': $res = "about"; break;
+      case 'contact': $res = "contact"; break;
+      case 'events': $res = "events"; break;
+      case 'gallery': $res = "gallery"; break;
       default: $res = "404"; break;
     } // switch end
     return $this->render('OChomePageBundle:Default:'.$res.'.html.twig');
   } // otherAction() end
+  
+  /*public function fileAction($folder,$file) {
+    switch($folder) {
+      case 'css': $folder = 'css'; break;
+      case 'fonts': $folder = 'fonts'; break;
+      case 'img': $folder = 'img'; break;
+      case 'js': $folder = 'js'; break;
+      default: $this->render('OChomePageBundle:Default:404.html.twig');
+    }
+    return $this->render('OChomePageBundle:Default:'.$folder.':'.$file);
+  }
+  
+  public function icoAction($file) {
+    return $this->render('OChomePageBundle:Default:img:icons:'.$file);
+  }*/
+  
 } // class end
