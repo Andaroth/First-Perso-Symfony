6ab3019346840235bde09db471e769cb476a3eac
Anda <ax.fiolle@gmail.com>
2018-02-06 10:15:39 +0100

    +globals(parameters.yml&config.yml) ~ templating
    Also fixed a vhost stuff

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -51,6 +51,35 @@ class AdvertController extends Controller
     return new Response($content);
   }
   
+  public function addAction(Request $request) {
+    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+    if ($request->isMethod('POST')) {
+      // Ici, on s'occupera de la création et de la gestion du formulaire
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+      // Puis on redirige vers la page de visualisation de cettte annonce
+      return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+    // Si on n'est pas en POST, alors on affiche le formulaire
+    return $this->render('OCPlatformBundle:Advert:add.html.twig');
+  }
+  
+  public function editAction($id, Request $request) {
+    // Ici, on récupérera l'annonce correspondante à $id
+    // Même mécanisme que pour l'ajout
+    if ($request->isMethod('POST')) {
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+      return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+    return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+  }
+  
+  public function deleteAction($id) {
+    // Ici, on récupérera l'annonce correspondant à $id
+    // Ici, on gérera la suppression de l'annonce en question
+    return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+  }
+  
   public function viewSlugAction($year,$slug,$format) {
     return new Response("Vous &ecirc;tes en ".$year.", sur la page \"".$slug.".".$format."\"");
   }
